07d8b2a1c506cc9f94d973aabdefa88bcf3760e5
unknown <bolivar014@gmail.com>
2019-07-07 17:01:11 -0500

    Migration Table Proveedores

---
+++ database/migrations/2019_07_07_215556_create_proveedores_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProveedoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proveedores', function (Blueprint $table) {
+            // $table->increments('id');
+            $table->integer('id')->unsigned();
+            $table->string('contacto', 50)->nullable();
+            $table->string('telefono', 50)->nullable();
+            // $table->timestamps();
+            
+            $table->foreign('id')->references('id')->on('personas')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proveedores');
+    }
+}
